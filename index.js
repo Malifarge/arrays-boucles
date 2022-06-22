8cf91d3c5b5b39003b3304fa13cdc102c617465a
Thomas <malifarge.thomas9@gmail.com>
2022-06-22 11:09:42 +0200

    Access '

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 const fruits = ["mango","lemon" ,"blueberry"]
 console.log(fruits);
 console.table(fruits)
+
+// 02 - Access
+
+const ingrédients = ["eff", "milk","butter"]
+console.log(ingrédients[1]);
+console.log(ingrédients.indexOf("butter"));
