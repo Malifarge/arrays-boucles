b44b18b00c566dd536d8fe6163bc6fa4734d18bb
Thomas <malifarge.thomas9@gmail.com>
2022-06-22 11:13:32 +0200

    Add and Remove

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ console.table(fruits)
 const ingrédients = ["eff", "milk","butter"]
 console.log(ingrédients[1]);
 console.log(ingrédients.indexOf("butter"));
+
+// 03 - Add and Remove
+
+const objects = ["pen", "book","lamp"]
+objects.unshift("chair")
+console.log(objects);
+objects.pop()
+console.log(objects);
+objects.push("laptop")
+console.log(objects);
+objects.shift();
+console.log(objects);
